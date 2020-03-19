352b15da3524c1de548dd4a24fc49d8d0a399933
Juraj Sic <juraj.sic@outlook.com>
2020-03-19 19:50:53 +0100

    initial commit

---
+++ tool-infos/setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='MyToolInfos',
+      version='1.0',
+      description='My tool-info files for tools for benchexec',
+      author='Juraj Sic',
+      packages=setuptools.find_packages(),
+     )
