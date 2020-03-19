3c452608e1049818bf08e9b60619005e0d988a6c
Juraj Sic <juraj.sic@outlook.com>
2020-03-19 22:24:48 +0100

    made mytoolinfos package explicit in setup.py

--- tool-infos/setup.py
+++ tool-infos/setup.py
@@ -6,5 +6,5 @@ setup(name='MyToolInfos',
       version='1.0',
       description='My tool-info files for tools for benchexec',
       author='Juraj Sic',
-      packages=setuptools.find_packages(),
+      packages=['mytoolinfos'],
      )
